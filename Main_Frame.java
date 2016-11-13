8ab7e3650f09fb675f9413fd1d926fe651a5fa34
Fredy Espana 15060 <fredy.espc@gmail.com>
2016-11-13 12:48:04 -0600

    Minimo

---
+++ Main_Frame.java
@@ -0,0 +1,54 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class Main_Frame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main_Frame frame = new Main_Frame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main_Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 599, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bienvenido a sus recomendaciones");
+		lblNewLabel.setBounds(10, 0, 175, 35);
+		panel.add(lblNewLabel);
+		
+		JLabel lblSeleccionaTuPlaylist = new JLabel("Selecciona tu Playlist");
+		lblSeleccionaTuPlaylist.setFont(new Font("Leelawadee UI", Font.PLAIN, 18));
+		lblSeleccionaTuPlaylist.setBounds(20, 32, 175, 59);
+		panel.add(lblSeleccionaTuPlaylist);
+	}
+}
